ad1f577b97cdd02c068e62c0dd6701cf1fbd7e9d
anton <anton.vorobev1999@mail.ru>
2023-05-28 19:17:08 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springboot/springhomework2/SpringHomework2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springhomework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHomework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHomework2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/springhomework2/SpringHomework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.springhomework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
